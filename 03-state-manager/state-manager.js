b9328aef6db53bed6698c72d9697caebc518ab64
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-05 12:27:22 +0530

    state manager implementation

---
+++ 03-state-manager/state-manager.js
@@ -0,0 +1,40 @@
+let StateManager = (function(){
+    let _currentState = undefined,
+        _callbacks = [],
+        _reducer = undefined,
+        _init_action = { type : '@@INIT/ACTION' };
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(callback){
+        if (typeof callback === 'function')
+            _callbacks.push(callback);
+    }
+
+    function triggerChange(){
+        _callbacks.forEach(callback => callback());
+    }
+
+    function dispatch(action){
+        let newState = _reducer(_currentState, action);
+        if (newState === _currentState) return;
+        _currentState = newState;
+        triggerChange();
+    }
+
+    function createStore(reducer){
+        if (typeof reducer !== 'function')
+            throw new Error('Invalid argument(s)');
+        _reducer = reducer;
+        
+        //to initialize the current state with a VALID DEFAULT state
+        _currentState = _reducer(undefined, _init_action);
+        const store = { getState, subscribe, dispatch }
+        return store;
+    }
+
+    return { createStore };
+
+})();
