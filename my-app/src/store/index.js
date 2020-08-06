a5e72096ffe87f35d624e92db6420dc451b59e21
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-06 13:46:57 +0530

    creating custom middlewares (loggerMiddleware)

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,14 +1,31 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import bugsReducer from '../bugTracker/reducers/bugsReducer';
 import spinnerReducer from '../spinner/reducers/spinnerReducer';
+
+function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.group('BEFORE ');
+            console.log(store.getState());
+            console.groupEnd();
+            console.log(action);
+            next(action);
+            console.group('AFTER');
+            console.log(store.getState());
+            console.groupEnd();
+            console.groupEnd();
+        }
+    }
+}
 
 const rootReducer = combineReducers({
     spinnerState : spinnerReducer,
     bugsState : bugsReducer
 });
 
-const appStore = createStore(rootReducer);
+const appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default appStore;
 
